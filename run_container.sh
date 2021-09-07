61a3f9b5792b5528321846ec29d144a0b94aad9e
fizwit <john@fuzzdog.com>
2021-09-07 14:12:50 -0700

    fix EasyBuild install script

--- run_container.sh
+++ run_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the container in test mode mode. Write software to local /app
-docker run --rm -ti --name 2019b \
+docker run --rm -ti --name 2020b \
  --detach \
  --security-opt seccomp=unconfined \
- fredhutch/ls2:eb-4.3.1-foss-2019b
+ fredhutch/ls2:eb-4.4.1-foss-2020b
--- scripts/install_easybuild.sh
+++ scripts/install_easybuild.sh
@@ -13,12 +13,11 @@ export BUILD_DIR=$4
 # update environment to use this temporary EasyBuild installation
 export PATH=$EB_TMPDIR/bin:$PATH
 export PYTHONPATH=$(/bin/ls -rtd -1 $EB_TMPDIR/lib*/python*/site-packages | tail -1):$PYTHONPATH
-export EB_PYTHON=python3
 
 echo "Loading Lmod..."
 source ${PREFIX}/lmod/lmod/init/profile
 module use ${PREFIX}/modules/all
-eb --install-latest-eb-release --prefix ${PREFIX}/easybuild --installpath-modules=${PREFIX}/modules
+eb --install-latest-eb-release --prefix ${PREFIX} --installpath-modules=${PREFIX}/modules
 
 echo "Customizing EasyBuild modulefile..."
 if [ -f "${PREFIX}/modules/all/EasyBuild/${EB_VER}.lua" ]
