1e10d6d63a34549047d0c7d0fec0ae6e15aae01c
fizwit <john@fuzzdog.com>
2024-10-17 11:35:16 -0700

    update EBcb Container

--- startEBcb.sh
+++ startEBcb.sh
@@ -36,7 +36,7 @@ echo Starting: ${containerName}  as $name
 docker run --gpus all --rm -ti --name $name --hostname $name \
  --security-opt="seccomp=unconfined" \
  --env EASYCONFIG=foo \
- -v /app:/app \
- -v ${SOURCE_DIR}:/sources \
- -v ${BUILD_DIR}:/build \
+ -v /app/software:/app/software \
+ -v /app/modules:/app/modules \
+ -v /app/sources:/sources \
  ${containerName} bash 
